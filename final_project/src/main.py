1a4c3e65a5265979f25faf21a2517b3fed9b3884
Baselm1 <103776402+Baselm1@users.noreply.github.com>
2024-06-30 23:45:55 +0300

    Final
    Added Data (not a lot of memory), deleted old project files.

--- final_project/src/main.py
+++
@@ -1,9 +0,0 @@
-########################################
-######## MAIN DRIVER SCRIPT ############
-########################################
-
-'''
-Description:
-    This file is the main driver of the project.
-    [Add more info as we go]
-'''
